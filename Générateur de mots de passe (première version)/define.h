2c14047c16fc8f83f5f0e14d9db9f267c6b5b5a5
Vincent Canu <72602625+MiddGuardd@users.noreply.github.com>
2022-06-08 14:54:30 +0200

    Update define.h

--- Générateur de mots de passe (première version)/define.h
+++ Générateur de mots de passe (première version)/define.h
@@ -5,9 +5,12 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
+//ON CREE LA STRUCTURE "MDP" donc mot de passe
 typedef struct{
+  //LA TAILLE VARIABLE DU MOT DE PASSE
   int taille;
   char texte[255];
+  //DIFFERENTES OPTIONS POUR AFINER LA CREATION
   int min;
   int max;
   int chiffre;
