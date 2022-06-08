1f0422356724a053136a819aec90bf88ca70b252
Vincent Canu <72602625+MiddGuardd@users.noreply.github.com>
2022-06-08 14:51:09 +0200

    Update fonctions.c

--- Générateur de mots de passe (première version)/fonctions.c
+++ Générateur de mots de passe (première version)/fonctions.c
@@ -4,7 +4,7 @@ char gen_min(){
   char renv;
   char min[26]="abcdefghijklmnopqrstuvwxyz";
 
-  renv = min[rand()%(26-1)];
+  renv = min[rand()%(25-0+1)+0];
 
   return renv;
 }
@@ -13,7 +13,7 @@ char gen_max(){
   char renv;
   char max[26]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  renv = max[rand()%(26-1)];
+  renv = max[rand()%(25-0+1)+0];
 
   return renv;
 }
@@ -22,7 +22,7 @@ char gen_chif(){
   char renv;
   char chif[10]="0123456789";
 
-  renv = chif[rand()%(10-1)];
+  renv = chif[rand()%(9-0+1)+0];
 
   return renv;
 }
@@ -31,7 +31,7 @@ char gen_spec(){
   char renv;
   char spec[14]="%_-!$^&#()[]=@";
 
-  renv = spec[rand()%(14-1)];
+  renv = spec[rand()%(13-0+1)+0];
 
   return renv;
 }
@@ -42,10 +42,10 @@ void gen(MDP *mot){
   for(int i=0; i<mot->taille; i++){
     int random=rand()%(4-0+1)+0;
 
-    if(random==1 && mot->min==1) mot->texte[i]=gen_min();
-    else if(random==2 && mot->max==1) mot->texte[i]=gen_max();
-    else if(random==3 && mot->chiffre==1) mot->texte[i]=gen_chif();
-    else if(random==4 && mot->spec==1)mot->texte[i]=gen_spec();
+    if(random==1 && mot->min==1) mot->texte[i] = gen_min();
+    else if(random==2 && mot->max==1) mot->texte[i] = gen_max();
+    else if(random==3 && mot->chiffre==1) mot->texte[i] = gen_chif();
+    else if(random==4 && mot->spec==1)mot->texte[i] = gen_spec();
     else i--;
   }
 }
