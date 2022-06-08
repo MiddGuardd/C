75e49fa264586f04e2fe9f165360fa48cf5fe92e
Vincent Canu <72602625+MiddGuardd@users.noreply.github.com>
2022-06-08 15:04:57 +0200

    Update fonctions.c

--- Générateur de mots de passe (première version)/fonctions.c
+++ Générateur de mots de passe (première version)/fonctions.c
@@ -1,5 +1,8 @@
 #include "fonctions.h"
 
+// ON CREE DES "TABLES" CONTENANT LES DIFFERENTS CARACTERES POUVANT ETRE UTILISES LORS DE LA GENERATION DU MOT DE PASSE
+// CHAQUE FONCTION VA RENVOYER UN CARACTERE ALEATOIRE DE LA TABLE
+
 char gen_min(){
   char renv;
   char min[26]="abcdefghijklmnopqrstuvwxyz";
@@ -36,20 +39,26 @@ char gen_spec(){
   return renv;
 }
 
+// LA FONCTION PRINCIPALE QUI PERMET LA GENERATION
+
 void gen(MDP *mot){
 
   
   for(int i=0; i<mot->taille; i++){
+    // ON DECIDE ALEATOIREMENT LA NATURE DE CHAQUE CARACTRE DU MOT DE PASSE (SI C'EST UN CHIFFRE, UNE LETTRE, ETC...)
     int random=rand()%(4-0+1)+0;
-
+    
+    // A CHAQUE FOIS ON REGARDE SI L'UTLISATEUR SOUHAITE BIEN QU'IL Y AIT CETTE CATEGORIE DE CARACTERES DANS SON MDP
     if(random==1 && mot->min==1) mot->texte[i] = gen_min();
     else if(random==2 && mot->max==1) mot->texte[i] = gen_max();
     else if(random==3 && mot->chiffre==1) mot->texte[i] = gen_chif();
     else if(random==4 && mot->spec==1)mot->texte[i] = gen_spec();
+    // ON REFAIT UN TOUR DE BOUCLE SINON (EXEMPLE: SI ALEATOIREMENT LE CARACTERE N°5 EST CENSE ETRE UN CHIFFRE MAIS QUE L'UTILISATEUR NE SOUAHITE PAS
+    // DE CHIFFRES DANS SON MDP ALORS ON REFAIT UN TOUR DE BOUCLE ET ON "TIRE" A NOUVEAU UNE CATEGORIE DE CARACTERES)
     else i--;
   }
 }
-
+// ON AFFICHE TOUT SIMPLEMENT LE MOT DE PASSE
 void print_mdp(MDP *mot){
   gen(mot);
   for(int i=0; i<mot->taille; i++){
